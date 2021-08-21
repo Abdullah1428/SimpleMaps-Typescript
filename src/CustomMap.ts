fda5698753eadba34bbce063ab50d751ee6b96b9
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-21 11:21:21 +0200

    one approach for refactor

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -14,16 +14,17 @@ export class CustomMap {
     })
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     })
   }
 
+  /*
   addCompanyMarker(company: Company): void {
     new google.maps.Marker({
       map: this.googleMap,
@@ -33,4 +34,5 @@ export class CustomMap {
       }
     })
   }
+  */
 }
