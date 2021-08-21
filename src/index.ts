6499cc178a834a1af704e1793b1bb139b96e45ae
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-21 11:16:36 +0200

    added methods for adding markers for user and company

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,9 @@ import { User } from './User'
 import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 
-new CustomMap('map')
+const user = new User()
+const company = new Company()
+const customMap = new CustomMap('map')
+
+customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
