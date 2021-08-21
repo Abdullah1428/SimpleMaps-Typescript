17cc5355c66db255e623bdaaa78599dfab4705b0
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-21 10:59:34 +0200

    displayed maps successfully

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-import { User } from './User'
-import { Company } from './Company'
+//import { User } from './User'
+//import { Company } from './Company'
 
-const user = new User()
-const company = new Company()
-
-console.log(user)
-console.log(company)
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
