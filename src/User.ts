484257989a3672ca13785386e235e2feb9d5e99b
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-21 11:42:33 +0200

    added method for marker content

--- src/User.ts
+++ src/User.ts
@@ -14,4 +14,12 @@ export class User {
       lng: parseFloat(faker.address.longitude())
     }
   }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>User Name: ${this.name}<h1>
+    <div>
+    `
+  }
 }
